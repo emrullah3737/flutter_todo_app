bf35313b2ae77ba391f73454f120dac7c077c682
emrullahkaratas <emrullahkaratas@yandex.com>
2020-06-13 00:03:04 +0300

    todo form validation and edit process added

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutterapp2/models/todo.dart';
 import 'package:flutterapp2/widgets/todo_list_item.dart';
 
+enum TodoFormType { add, edit }
+
 class Home extends StatefulWidget {
   @override
   _HomeState createState() => _HomeState();
@@ -9,10 +11,13 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
   final List<Todo> todoList = [];
+  final GlobalKey<FormState> todoFormKey = GlobalKey<FormState>();
   final TextEditingController titleController = TextEditingController();
   final TextEditingController contentController = TextEditingController();
 
   void addTodo(BuildContext context) {
+    if (!validateTodoForm()) return;
+
     setState(() {
       todoList.add(
           Todo(title: titleController.text, content: contentController.text));
@@ -20,59 +25,121 @@ class _HomeState extends State<Home> {
     Navigator.pop(context);
   }
 
-  void deleteTodo(Todo todo) {
+  void updateTodo(BuildContext context, Todo activeTodo) {
+    if (!validateTodoForm()) return;
+
+    setState(() {
+      activeTodo.title = titleController.text;
+      activeTodo.content = contentController.text;
+      activeTodo.updatedAt = DateTime.now();
+    });
+    Navigator.pop(context);
+  }
+
+  void onPressDeleteTodo(Todo todo) {
     setState(() {
       todoList.remove(todo);
     });
   }
 
-  void onPressAddTodo() {
-    showDialog(
-        context: context,
-        builder: (BuildContext context) {
-          return SimpleDialog(
-            backgroundColor: Colors.white,
-            title: Text('New Todo'),
-            children: <Widget>[
-              Container(
-                margin: EdgeInsets.fromLTRB(16, 10, 16, 10),
-                child: Column(
-                  children: <Widget>[
-                    TextField(
-                      controller: titleController,
-                      maxLength: 10,
-                      decoration: InputDecoration(
-                        border: OutlineInputBorder(),
-                        labelText: 'Title',
-                      ),
-                    ),
-                    TextField(
-                      controller: contentController,
-                      keyboardType: TextInputType.multiline,
-                      maxLines: null,
-                      minLines: 5,
-                      decoration: InputDecoration(
-                        border: OutlineInputBorder(),
-                        alignLabelWithHint: true,
-                        labelText: 'Content',
-                      ),
-                    ),
-                    Divider(height: 20),
-                    RaisedButton(
-                      onPressed: () => addTodo(context),
-                      color: Colors.redAccent,
-                      child: Text('Add Todo',
-                          style: TextStyle(color: Colors.white)),
-                    ),
-                  ],
-                ),
-              ),
-            ],
-          );
-        }).then((value) => clearAddTodoForm());
+  void onPressEditTodo(Todo todo) {
+    titleController.text = todo.title;
+    contentController.text = todo.content;
+    showTodoFormDialog(
+      todoFormType: TodoFormType.edit,
+      activeTodo: todo,
+    );
   }
 
-  void clearAddTodoForm() {
+  void onPressAddTodo() {
+    showTodoFormDialog(
+      todoFormType: TodoFormType.add,
+    );
+  }
+
+  bool validateTodoForm() {
+    return todoFormKey.currentState.validate();
+  }
+
+  String emptyValidator(value) {
+    if (value.isEmpty) {
+      return 'Field Required!';
+    }
+    return null;
+  }
+
+  void showTodoFormDialog({TodoFormType todoFormType, Todo activeTodo}) {
+    final String dialogTitle =
+        todoFormType == TodoFormType.add ? 'New Todo' : 'Edit Todo';
+    final String dialogButton =
+        todoFormType == TodoFormType.add ? 'Add Todo' : 'Confirm Todo';
+
+    showDialog(
+        context: context,
+        builder: (BuildContext buildContext) {
+          return Form(
+            key: todoFormKey,
+            child: SimpleDialog(
+              backgroundColor: Colors.white,
+              title: Center(child: Text(dialogTitle)),
+              children: <Widget>[
+                Divider(height: 20),
+                Container(
+                  margin: EdgeInsets.fromLTRB(16, 10, 16, 10),
+                  child: Column(
+                    children: <Widget>[
+                      TextFormField(
+                        textCapitalization: TextCapitalization.characters,
+                        validator: emptyValidator,
+                        controller: titleController,
+                        style: TextStyle(fontSize: 20),
+                        decoration: InputDecoration(
+                          alignLabelWithHint: true,
+                          labelText: 'Title',
+                        ),
+                        maxLength: 10,
+                      ),
+                      SizedBox(height: 20),
+                      TextFormField(
+                        textCapitalization: TextCapitalization.sentences,
+                        validator: emptyValidator,
+                        controller: contentController,
+                        keyboardType: TextInputType.multiline,
+                        maxLines: null,
+                        minLines: 5,
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(),
+                          alignLabelWithHint: true,
+                          labelText: 'Content',
+                        ),
+                      ),
+                      Divider(height: 20),
+                      RaisedButton(
+                        onPressed: () {
+                          if (todoFormType == TodoFormType.add) {
+                            addTodo(buildContext);
+                            return;
+                          }
+
+                          if (todoFormType == TodoFormType.edit) {
+                            updateTodo(buildContext, activeTodo);
+                            return;
+                          }
+                        },
+                        color: Colors.redAccent,
+                        child: Text(dialogButton,
+                            style: TextStyle(color: Colors.white)),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          );
+        }).then((value) => clearTodoForm());
+  }
+
+  void clearTodoForm() {
     titleController.clear();
     contentController.clear();
   }
@@ -86,18 +153,21 @@ class _HomeState extends State<Home> {
 
   @override
   Widget build(BuildContext context) {
+    todoList.sort((b, a) => a.createdAt.compareTo(b.createdAt));
+
     return Scaffold(
       body: SafeArea(
         child: Container(
-          color: Colors.black,
+          color: Colors.black87,
           child: Center(
             child: Container(
-              margin: EdgeInsets.fromLTRB(16, 0, 16, 10),
+              margin: EdgeInsets.fromLTRB(16, 10, 16, 10),
               child: ListView(
                 children: todoList
                     .map((Todo todo) => TodoListItem(
                           todo: todo,
-                          onPressDelete: deleteTodo,
+                          onPressDelete: onPressDeleteTodo,
+                          onPressEdit: onPressEditTodo,
                         ))
                     .toList(),
               ),
--- lib/widgets/todo_list_item.dart
+++ lib/widgets/todo_list_item.dart
@@ -1,5 +1,8 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutterapp2/models/todo.dart';
+import 'package:intl/intl.dart';
+import 'package:timeago/timeago.dart' as timeago;
 
 enum TodoOptions { delete, edit }
 
@@ -7,11 +10,13 @@ class TodoListItem extends StatelessWidget {
   final Todo todo;
   final double bottomMargin;
   final Function onPressDelete;
+  final Function onPressEdit;
 
   const TodoListItem({
     this.todo,
     this.bottomMargin = 0,
     this.onPressDelete,
+    this.onPressEdit,
   });
 
   void onSelectTodoOption(TodoOptions todoOptions) {
@@ -21,9 +26,22 @@ class TodoListItem extends StatelessWidget {
     }
 
     if (todoOptions == TodoOptions.edit) {
-      // TODO: update process
+      onPressEdit(todo);
       return;
     }
+  }
+
+  String formattedUpdatedAtDate() {
+    final DateTime now = DateTime.now();
+    final Duration diff = now.difference(todo.updatedAt);
+
+    return timeago.format(now.subtract(diff));
+  }
+
+  String formattedCreatedAtDate() {
+    DateFormat formatter = DateFormat('dd MMM yyyy');
+
+    return formatter.format(todo.createdAt);
   }
 
   @override
@@ -31,12 +49,26 @@ class TodoListItem extends StatelessWidget {
     return Container(
       margin: EdgeInsets.fromLTRB(0, 0, 0, bottomMargin),
       child: Card(
-        color: Colors.redAccent,
+        color: Colors.white,
         child: Container(
           padding: EdgeInsets.all(10),
           child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: <Widget>[
+                Flex(
+                  direction: Axis.horizontal,
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      formattedCreatedAtDate(),
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 8,
+                      ),
+                    )
+                  ],
+                ),
                 Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: <Widget>[
@@ -48,7 +80,7 @@ class TodoListItem extends StatelessWidget {
                           fontWeight: FontWeight.bold,
                           fontSize: 20,
                           letterSpacing: 1,
-                          color: Colors.amberAccent,
+                          color: Colors.black,
                         ),
                       ),
                     ),
@@ -72,7 +104,7 @@ class TodoListItem extends StatelessWidget {
                             children: <Widget>[
                               Icon(Icons.edit),
                               SizedBox(width: 10),
-                              Text('Update Todo'),
+                              Text('Edit Todo'),
                             ],
                           ),
                         )
@@ -86,8 +118,22 @@ class TodoListItem extends StatelessWidget {
                 Text(
                   todo.content,
                   style: TextStyle(
-                      fontSize: 12, letterSpacing: 1, color: Colors.white),
-                )
+                      fontSize: 12, letterSpacing: 1, color: Colors.black87),
+                ),
+                SizedBox(height: 30),
+                Flex(
+                  direction: Axis.horizontal,
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: <Widget>[
+                    Text(
+                      'Last updated ${formattedUpdatedAtDate()}',
+                      style: TextStyle(
+                        color: Colors.black54,
+                        fontSize: 8,
+                      ),
+                    )
+                  ],
+                ),
               ]),
         ),
       ),
